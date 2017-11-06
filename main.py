c2235db0681ddb45ed6713cafb72339c5451bf60
Raphael H Ribas <raphael.ribas@gmail.com>
2017-11-05 23:35:32 -0800

    Remove some features.

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class Params:
             'task': 'train',
             'boosting_type': 'gbdt',
             'objective': 'binary',
-            'metric': 'auc',
+            'metric': 'binary_logloss',
             'learning_rate': self.eta,
             'feature_fraction': self.colsample,
             'bagging_fraction': self.subsample,
@@ -140,7 +140,7 @@ def CrossValidate(train, test, target, variables, folds=5, params=Params()):
         merge_dicts(importance, imp)
 
         val_pred = predictor(sub_val)
-        cv = CostFunction(sub_val[target], val_pred)
+        cv = CostFunction(np.array(sub_val[target]), np.array(val_pred))
         loss.append(cv)
         train_preds[test_index] = val_pred
 
@@ -461,10 +461,11 @@ def gen_specialized_features(train, test, val, target):
 
     return train, test, val, features
 
+
 @log_duration
 def preCVFeatures(train, test):
     combs = [
-        ('ps_reg_01', 'ps_car_02_cat'),  
+        ('ps_reg_01', 'ps_car_02_cat'),
         ('ps_reg_01', 'ps_car_04_cat'),
     ]
 
@@ -472,20 +473,23 @@ def preCVFeatures(train, test):
 
     for n_c, (f1, f2) in enumerate(combs):
         name1 = f1 + "_plus_" + f2
-        print('current feature %60s %4d in %5.1f'
-              % (name1, n_c + 1, (time.time() - start) / 60), end='')
-        print('\r' * 75, end='')
-        train_df[name1] = train_df[f1].apply(lambda x: str(x)) + "_" + train_df[f2].apply(lambda x: str(x))
-        test_df[name1] = test_df[f1].apply(lambda x: str(x)) + "_" + test_df[f2].apply(lambda x: str(x))
+        print('current feature %60s %4d' % (name1, n_c + 1))
+
+        train[name1] = (
+                train[f1].apply(lambda x: str(x)) + "_" +
+                train[f2].apply(lambda x: str(x)))
+        test[name1] = (
+                test[f1].apply(lambda x: str(x)) + "_" +
+                test[f2].apply(lambda x: str(x)))
         # Label Encode
         lbl = LabelEncoder()
-        lbl.fit(list(train_df[name1].values) + list(test_df[name1].values))
-        train_df[name1] = lbl.transform(list(train_df[name1].values))
-        test_df[name1] = lbl.transform(list(test_df[name1].values))
+        lbl.fit(list(train[name1].values) + list(test[name1].values))
+        train[name1] = lbl.transform(list(train[name1].values))
+        test[name1] = lbl.transform(list(test[name1].values))
 
         features.append(name1)
 
-    return traind_df, test_df, features
+    return train, test, features
 
 
 @log_duration
@@ -546,7 +550,7 @@ def main():
     parser.add_argument('--lambda-l1', default=8, type=float)
     parser.add_argument('--lambda-l2', default=1, type=float)
     parser.add_argument('--scale-pos-weight', default=1.6, type=float)
-    parser.add_argument('--min-split-gain', default=0.1, type=float)
+    parser.add_argument('--min-split-gain', default=1, type=float)
     args = parser.parse_args()
 
     random.seed(SEED)
@@ -561,70 +565,106 @@ def main():
     test = gen_features(load_test_data())
 
     feats = [
-      'ps_ind_01',
-      'ps_ind_02_cat',
-      'ps_ind_03',
-      'ps_ind_04_cat',
-      'ps_ind_05_cat',
-      'ps_ind_06_bin',
-      'ps_ind_07_bin',
-      'ps_ind_08_bin',
-      'ps_ind_09_bin',
-      'ps_ind_10_bin',
-      'ps_ind_11_bin',
-      'ps_ind_12_bin',
-      'ps_ind_13_bin',
-      'ps_ind_14',
-      'ps_ind_15',
-      'ps_ind_16_bin',
-      'ps_ind_17_bin',
-      'ps_ind_18_bin',
-      'ps_reg_01',
-      'ps_reg_02',
-      'ps_reg_03',
-      'ps_car_01_cat',
-      'ps_car_02_cat',
-      'ps_car_03_cat',
-      'ps_car_04_cat',
-      'ps_car_05_cat',
-      'ps_car_06_cat',
-      'ps_car_07_cat',
-      'ps_car_08_cat',
-      'ps_car_09_cat',
-      'ps_car_10_cat',
-      'ps_car_11_cat',
-      'ps_car_11',
-      'ps_car_12',
-      'ps_car_13',
-      'ps_car_14',
-      'ps_car_15',
-      'ps_calc_01',
-      'ps_calc_02',
-      'ps_calc_03',
-      'ps_calc_04',
-      'ps_calc_05',
-      'ps_calc_06',
-      'ps_calc_07',
-      'ps_calc_08',
-      'ps_calc_09',
-      'ps_calc_10',
-      'ps_calc_11',
-      'ps_calc_12',
-      'ps_calc_13',
-      'ps_calc_14',
-      'ps_calc_15_bin',
-      'ps_calc_16_bin',
-      'ps_calc_17_bin',
-      'ps_calc_18_bin',
-      'ps_calc_19_bin',
-      'ps_calc_20_bin',
+      # 'ps_ind_01',
+      # 'ps_ind_02_cat',
+      # 'ps_ind_03',
+      # 'ps_ind_04_cat',
+      # 'ps_ind_05_cat',
+      # 'ps_ind_06_bin',
+      # 'ps_ind_07_bin',
+      # 'ps_ind_08_bin',
+      # 'ps_ind_09_bin',
+      # 'ps_ind_10_bin',
+      # 'ps_ind_11_bin',
+      # 'ps_ind_12_bin',
+      # 'ps_ind_13_bin',
+      # 'ps_ind_14',
+      # 'ps_ind_15',
+      # 'ps_ind_16_bin',
+      # 'ps_ind_17_bin',
+      # 'ps_ind_18_bin',
+      # 'ps_reg_01',
+      # 'ps_reg_02',
+      # 'ps_reg_03',
+      # 'ps_car_01_cat',
+      # 'ps_car_02_cat',
+      # 'ps_car_03_cat',
+      # 'ps_car_04_cat',
+      # 'ps_car_05_cat',
+      # 'ps_car_06_cat',
+      # 'ps_car_07_cat',
+      # 'ps_car_08_cat',
+      # 'ps_car_09_cat',
+      # 'ps_car_10_cat',
+      # 'ps_car_11_cat',
+      # 'ps_car_11',
+      # 'ps_car_12',
+      # 'ps_car_13',
+      # 'ps_car_14',
+      # 'ps_car_15',
+      # 'ps_calc_01',
+      # 'ps_calc_02',
+      # 'ps_calc_03',
+      # 'ps_calc_04',
+      # 'ps_calc_05',
+      # 'ps_calc_06',
+      # 'ps_calc_07',
+      # 'ps_calc_08',
+      # 'ps_calc_09',
+      # 'ps_calc_10',
+      # 'ps_calc_11',
+      # 'ps_calc_12',
+      # 'ps_calc_13',
+      # 'ps_calc_14',
+      # 'ps_calc_15_bin',
+      # 'ps_calc_16_bin',
+      # 'ps_calc_17_bin',
+      # 'ps_calc_18_bin',
+      # 'ps_calc_19_bin',
+      # 'ps_calc_20_bin',
+
+
+      "ps_car_13",  #            : 1571.65 / shadow  609.23
+      "ps_reg_03",  #            : 1408.42 / shadow  511.15
+      "ps_ind_05_cat",  #        : 1387.87 / shadow   84.72
+      "ps_ind_03",  #            : 1219.47 / shadow  230.55
+      "ps_ind_15",  #            :  922.18 / shadow  242.00
+      "ps_reg_02",  #            :  920.65 / shadow  267.50
+      "ps_car_14",  #            :  798.48 / shadow  549.58
+      "ps_car_12",  #            :  731.93 / shadow  293.62
+      "ps_car_01_cat",  #        :  698.07 / shadow  178.72
+      "ps_car_07_cat",  #        :  694.53 / shadow   36.35
+      "ps_ind_17_bin",  #        :  620.77 / shadow   23.15
+      "ps_car_03_cat",  #        :  611.73 / shadow   50.67
+      "ps_reg_01",  #            :  598.60 / shadow  178.57
+      "ps_car_15",  #            :  593.35 / shadow  226.43
+      "ps_ind_01",  #            :  547.32 / shadow  154.58
+      "ps_ind_16_bin",  #        :  475.37 / shadow   34.17
+      "ps_ind_07_bin",  #        :  435.28 / shadow   28.92
+      "ps_car_06_cat",  #        :  398.02 / shadow  212.43
+      "ps_car_04_cat",  #        :  376.87 / shadow   76.98
+      "ps_ind_06_bin",  #        :  370.97 / shadow   36.13
+      "ps_car_09_cat",  #        :  214.12 / shadow   81.38
+      "ps_car_02_cat",  #        :  203.03 / shadow   26.67
+      "ps_ind_02_cat",  #        :  189.47 / shadow   65.68
+      "ps_car_11",  #            :  173.28 / shadow   76.45
+      "ps_car_05_cat",  #        :  172.75 / shadow   62.92
+      "ps_calc_09",  #           :  169.13 / shadow  129.72
+      "ps_calc_05",  #           :  148.83 / shadow  120.68
+      "ps_ind_08_bin",  #        :  140.73 / shadow   27.63
+      "ps_car_08_cat",  #        :  120.87 / shadow   28.82
+      "ps_ind_09_bin",  #        :  113.92 / shadow   27.05
+      "ps_ind_04_cat",  #        :  107.27 / shadow   37.43
+      "ps_ind_18_bin",  #        :   77.42 / shadow   25.97
+      "ps_ind_12_bin",  #        :   39.67 / shadow   15.52
+      "ps_ind_14",  #            :   37.37 / shadow   16.65
     ]
 
     TARGET = 'target'
     ID_COLUMN = 'id'
 
-    train, test, f = preCVFeatures(train, test)
-    feats.extend(f)
+    # train, test, f = preCVFeatures(train, test)
+    # feats.extend(f)
 
     if args.search:
         params_space = dict(
